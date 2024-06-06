858076501aabf6c6c5e1ee28b9d3e40cb1883ad8
thony376 <ancreft@gmail.com>
2024-06-06 14:16:09 -0300

    primeiro envio: apresentação

---
+++ teste github/teste github.py
@@ -0,0 +1,6 @@
+nome= "Anthony Mendes Moyas"
+idade = 17
+cidade = "Mogi das Cruzes"
+formação = "Cursando python"
+
+print(f"Meu nome é: {nome}, tenho {idade}anos, moro em {cidade} e estou {formação}")
