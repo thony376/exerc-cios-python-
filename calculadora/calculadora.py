46eed2b7e3206f34f826d224fac60725244d2848
Anthony Moyas <ancreft@gmail.com>
2024-06-26 14:49:08 -0300

    Add files via upload

---
+++ calculadora/calculadora.py
@@ -0,0 +1,46 @@
+#multiplicação
+def tabuada(numero):
+    print(f"Tabuada do {numero}:")
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
+        print(resultado)
+        
+        
+tabuada(20)
+
+
+#soma
+def tabuada(numero):
+    print(f"Tabuada do {numero}:")
+    for i in range(1, 11):
+        resultado = numero + i
+        print(f"{numero} + {i} = {resultado}")
+        
+        
+tabuada(20)
+    
+
+#subtração
+def tabuada(numero):
+    print(f"Tabuada do {numero}:")
+    for i in range(1, 11):
+        resultado = numero - i
+        print(f"{numero} - {i} = {resultado}")
+        
+        
+tabuada(20)
+
+
+
+#divisão
+def tabuada(numero):
+    print(f"Tabuada do {numero}:")
+    for i in range(1, 11):
+        resultado = numero // i
+        print(f"{numero} // {i} = {resultado}")
+        
+        
+tabuada(20)
+
+    
